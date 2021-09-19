702e489a0de18b2c731cfce74c01419ff59144a3
Carlos Palacios <caappr@hotmail.com>
2021-09-19 15:58:29 -0500

    CRUD Clientes completo

--- src/main/java/clientes/Clientes.java
+++ src/main/java/clientes/Clientes.java
@@ -3,6 +3,7 @@ package clientes;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.swing.*;
 import java.io.IOException;
 
 @WebServlet(name = "Clientes", value = "/Clientes")
@@ -26,12 +27,62 @@ public class Clientes extends HttpServlet {
             phone = request.getParameter("phone");
 
             ClientesDTO clientsDTO = new ClientesDTO(cedula, address, email, name, phone);
-            if (clientsDAO.insertUser(clientsDTO)) {
-                response.sendRedirect("users.jsp?msg=Cliente agregado");
+            if (clientsDAO.insertclient(clientsDTO)) {
+                response.sendRedirect("clients.jsp?msg=Cliente agregado");
             } else {
-                response.sendRedirect("users.jsp?msg=Error al agregar");
+                response.sendRedirect("clients.jsp?msg=Error al agregar");
             }
         }
+        if (request.getParameter("search")!=null) {
+            int cedula;
+            String address, email, name, phone;
+            cedula = Integer.parseInt(request.getParameter("cedula"));
+            ClientesDTO client = clientsDAO.searchCliente(cedula);
+            if (client != null) {
+                cedula = client.getCedula();
+                address = client.getAdress();
+                email = client.getEmail();
+                name = client.getName();
+                phone = client.getPhone();
+                response.sendRedirect("clients.jsp?cedula=" + cedula +
+                        "&&address=" + address + "&&email=" + email + "&&name=" + name + "&&phone=" + phone);
+            } else {
+                response.sendRedirect("clients.jsp?msg= Cliente no encontrado");
+            }
+
+        }
+        if (request.getParameter("update") !=null){
+            int cedula;
+            String address, email,name,phone;
+            cedula = Integer.parseInt(request.getParameter("cedula"));
+            address = request.getParameter("address");
+            email = request.getParameter("email");
+            name = request.getParameter("name");
+            phone = request.getParameter("phone");
+
+            ClientesDTO clientsDTO = new ClientesDTO(cedula, address, email, name, phone);
+            if (clientsDAO.updateClient(clientsDTO)) {
+                response.sendRedirect("clients.jsp?msg=Cliente modificado");
+            } else {
+                response.sendRedirect("clients.jsp?msg=Error al modificado");
+            }
+
+        }
+        if (request.getParameter("delete")!=null){
+            int cedula;
+            cedula = Integer.parseInt(request.getParameter("cedula"));
+            int accept = JOptionPane.showInternalConfirmDialog(null,"¿Eliminar cliente con cedula: ?"+cedula);
+            if (accept == 0){
+                if (clientsDAO.deleteclient(cedula)){
+                    response.sendRedirect("clients.jsp?msg=Cliente eliminado");
+                } else {
+                    response.sendRedirect("clients.jsp?msg=Error al eliminar");
+                }
+            }else {
+                response.sendRedirect("clients.jsp");
+            }
+        }
+
 
     }
 }
--- src/main/java/clientes/ClientesDAO.java
+++ src/main/java/clientes/ClientesDAO.java
@@ -5,14 +5,16 @@ import Conexion.Conexion;
 import javax.swing.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ClientesDAO {
     Conexion conexion = new Conexion();
     Connection con = conexion.connect();
     PreparedStatement statement = null;
+    ResultSet resultSet = null;
 
-    public boolean insertUser(ClientesDTO clients){
+    public boolean insertclient(ClientesDTO clients){
         boolean result = false;
         try{
             String query = "insert into clientes values(?,?,?,?,?)";
@@ -29,4 +31,55 @@ public class ClientesDAO {
         }
         return result;
     }
+    public ClientesDTO searchCliente(int cedula) {
+        ClientesDTO cliente = null;
+        try {
+            String query = "select * from clientes where cedula_cliente=?";
+            statement = con.prepareStatement(query);
+            statement.setInt(1,cedula);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                cliente = new ClientesDTO(Integer.parseInt(resultSet.getString(1)),resultSet.getString(2),
+                        resultSet.getString(3),resultSet.getString(4), resultSet.getString(5));
+
+            }
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error"+e);
+        }
+        return cliente;
+    }
+
+    public boolean updateClient(ClientesDTO clientsDTO) {
+        boolean result = false;
+        try{
+            String query = "update clientes set direcion_cliente=?, email_cliente=?, nombre_cliente=?," +
+                    "telefono_clientes=? where cedula_cliente=?";
+            statement = con.prepareStatement(query);
+            statement.setString(1, clientsDTO.getAdress());
+            statement.setString(2, clientsDTO.getEmail());
+            statement.setString(3, clientsDTO.getName());
+            statement.setString(4, clientsDTO.getPhone());
+            statement.setInt(5,clientsDTO.getCedula());
+            result = statement.executeUpdate() > 0;
+
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error"+e);
+        }
+        return result;
+    }
+
+
+    public boolean deleteclient(int cedula) {
+        boolean result = false;
+        try{
+            String query = "delete FROM clientes WHERE cedula_cliente=?";
+            statement= con.prepareStatement(query);
+            statement.setInt(1, cedula);
+            result = statement.executeUpdate() > 0;
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error"+e);
+        }
+        return result;
+    }
+
 }
